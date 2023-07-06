8b63598a4a335928d5ddf01c0614afce53bf6dd3
RendyAFS <99011571+RendyAFS@users.noreply.github.com>
2023-07-06 18:39:22 +0700

    Add files via upload

---
+++ storage/framework/views/13426988adb53416ba1d0d858c0fa6fe.php
@@ -0,0 +1,15 @@
+<div class="d-flex">
+    <a href="<?php echo e(route('employees.show', ['employee' => $employee->id])); ?>" class="btn btn-outline-dark btn-sm me-2"><i class="bi-person-lines-fill"></i></a>
+    <a href="<?php echo e(route('employees.edit', ['employee' => $employee->id])); ?>" class="btn btn-outline-dark btn-sm me-2"><i class="bi-pencil-square"></i></a>
+
+    <div>
+        <form action="<?php echo e(route('employees.destroy', ['employee' => $employee->id])); ?>" method="POST">
+            <?php echo csrf_field(); ?>
+            <?php echo method_field('delete'); ?>
+            <button type="submit" class="btn btn-outline-dark btn-sm me-2 btn-delete" data-name="<?php echo e($employee->firstname.' '.$employee->lastname); ?>">
+                <i class="bi-trash"></i>
+            </button>
+        </form>
+    </div>
+</div>
+<?php /**PATH D:\----- 1. Kuliah\2. Bahan - bahan Kuliah\✨ SEMESTER 4\PEMROGRAMAN FRAMEWORK - IS-04-04\Pertemuan 12\modul-10\resources\views/employee/actions.blade.php ENDPATH**/ ?>
